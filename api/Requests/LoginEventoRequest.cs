66730104eeaeb790c74b14333b9b8f010346301c
fmacedosantos <felipemacedo986@gmail.com>
2024-05-24 11:55:32 -0300

    Desenvolvendo a lógica de requests.

---
+++ api/Requests/LoginEventoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Requests
+{
+    public class LoginEventoRequest
+    {
+        public string EmailContato { get; set; }
+        public int IdEvento { get; set; }
+    }
+}
---
+++ api/Requests/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Requests
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
