ecc3bee9e3616b440a28b6c107521602be967f45
Felipe Macedo dos Santos <89111798+fmacedosantos@users.noreply.github.com>
2024-06-04 15:04:03 -0300

    Mudando a conexão para a nuvem.

--- api/Program.cs
+++ api/Program.cs
@@ -4,7 +4,7 @@ using TickUp.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = "ConexaoPadrao";
+var connectionString = "jawsdb";
 builder.Services.AddSingleton<UsuarioService>(new UsuarioService(connectionString));
 builder.Services.AddSingleton<EventoService>(new EventoService(connectionString));
 builder.Services.AddSingleton<IngressoService>(new IngressoService(connectionString));
