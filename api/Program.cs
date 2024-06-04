bc7763a6a0209b2473512bdb01bd2d17c825480d
Felipe Macedo dos Santos <89111798+fmacedosantos@users.noreply.github.com>
2024-06-04 14:49:26 -0300

    Modificando a conexão.

--- api/Program.cs
+++ api/Program.cs
@@ -4,7 +4,7 @@ using TickUp.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = "jawsdb";
+var connectionString = "ConexaoPadrao";
 builder.Services.AddSingleton<UsuarioService>(new UsuarioService(connectionString));
 builder.Services.AddSingleton<EventoService>(new EventoService(connectionString));
 builder.Services.AddSingleton<IngressoService>(new IngressoService(connectionString));
